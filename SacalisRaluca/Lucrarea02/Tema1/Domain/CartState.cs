60fb56d7f209557d46363ba78f98181d6f31a64b
sacaaa03 <raluca.sacalis@yahoo.com>
2021-10-20 23:01:07 +0300

    Tema

---
+++ SacalisRaluca/Lucrarea02/Tema1/Domain/CartState.cs
@@ -0,0 +1,19 @@
+﻿using CSharp.Choices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01.Domain
+{
+    [AsChoice]
+    public static partial class CartState
+    {
+        public interface ICartState { }
+        public record EmptyCart(Cart Cart) : ICartState;
+        public record InvalidCart(Cart Cart, string Message) : ICartState;
+        public record ValidCart(Cart Cart) : ICartState;
+        public record PaidCart() : ICartState;
+    }
+}
---
+++ SacalisRaluca/Lucrarea02/Tema1/Domain/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01.Fake
+{
+    public class Product
+    {
+        public Product(int id, string name, float price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
