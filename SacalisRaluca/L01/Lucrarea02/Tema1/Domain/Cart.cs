669d12f05601a444f975af93a85a80dd5c67665c
sacaaa03 <91938018+sacaaa03@users.noreply.github.com>
2021-10-20 23:18:41 +0300

    Add files via upload

---
+++ SacalisRaluca/L01/Lucrarea02/Tema1/Domain/Cart.cs
@@ -0,0 +1,15 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using L01.Fake;
+
+namespace L01.Domain
+{
+    public record Cart(IEnumerable<(Product Product, int Quantity)> Items)
+    {
+        public Cart() : this(Enumerable.Empty<(Product, int)>()) { }
+    }
+}
---
+++ SacalisRaluca/L01/Lucrarea02/Tema1/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01.Extensions
+{
+    public record StringMultiplication(int Value)
+    {
+        public static implicit operator StringMultiplication(int value)
+            => new StringMultiplication(value);
+        public static implicit operator int(StringMultiplication value)
+            => value.Value;
+        public static string operator *(string @base, StringMultiplication count)
+            => Enumerable.Repeat(@base, count.Value).Aggregate((a, b) => a + b);
+        public static string operator *(StringMultiplication count, string @base)
+            => Enumerable.Repeat(@base, count.Value).Aggregate((a, b) => a + b);
+    }
+}
